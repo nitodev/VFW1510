d041f69a542b5a838623b35e0a1b873ff353cc55
nitodev <Naito@Nowas-MacBook-Pro.local>
2015-10-21 19:56:24 -0400

    added table view - project 4

--- Ito_Nowa_Project4/Resources/data.js
+++ Ito_Nowa_Project4/Resources/data.js
@@ -10,50 +10,48 @@ var galaxies = {
 	named : [
 		{
 			title: 'Andromeda Galaxy',
-			description: 'It is the closest big galaxy to the milky way.',
-			image: ''
+			description: 'It is the closest big galaxy to the milky way.'
 		},{
 			title: 'Comet Galaxy',
-			description: 'The comet effect is caused by tidal stripping by its galaxy cluster.',
-			image: ''
+			description: 'The comet effect is caused by tidal stripping by its galaxy cluster.'
 		},{
 			title: 'Cosmos Redshift 7',
-			description: 'It\'s reported to be the brightest of distant galaxies.',
-			image: ''
+			description: 'It\'s reported to be the brightest of distant galaxies.'
 		},{
 			title: 'Hoag\'s Object',
-			description: 'It\'s a polar-ring galaxy with the ring in the plane of rotation of the central object.',
-			image: ''
+			description: 'It\'s a polar-ring galaxy with the ring in the plane of rotation of the central object.'
 		},{
 			title: 'Large Megallanic Cloud',
-			description: 'This is the fourth largest galaxy in the local group.',
-			image: ''
+			description: 'This is the fourth largest galaxy in the local group.'
 		},{
 			title: 'Mayall\'s Object',
-			description: 'A peculiar looking object, likely to be two galaxies undergoing a collision.',
-			image: ''
+			description: 'A peculiar looking object, likely to be two galaxies undergoing a collision.'
 		},{
 			title: 'Tadpole Galaxy',
-			description: 'The shape resulted from tidal interaction that drew out a long tidal tail.',
-			image: ''
+			description: 'The shape resulted from tidal interaction that drew out a long tidal tail.'
 		}
 	],
 	nakedEye : [
 		{
 			title: 'Milky Way',
-			description: 'Our galaxy; most visible object in the sky are part of it.',
-			image: ''
+			description: 'Our galaxy; most visible object in the sky are part of it.'
 		},{
 			title: 'Small Magellanic Cloud',
-			description: 'Visible only from the southern hemisphere.',
-			image: ''
+			description: 'Visible only from the southern hemisphere.'
 		},{
 			title: 'Triangulum',
-			description: 'Being a diffuse object, its visibility is strongly affected by small amounts of light pollution.',
-			image: ''
+			description: 'Being a diffuse object, its visibility is strongly affected by small amounts of light pollution.'
 		}
 	]
 };
+
+for (i = 0; i < galaxies.named.length; i++){
+	galaxies.named[i].image = 'galaxies/' + galaxies.named[i].title + '.jpg';
+}
+for (c = 0; c < galaxies.nakedEye.length; c++){
+	galaxies.nakedEye[c].image = 'galaxies/' + galaxies.nakedEye[c].title + '.jpg';
+}
+
 
 
 exports.galaxies = galaxies;
--- Ito_Nowa_Project4/Resources/table.js
+++ Ito_Nowa_Project4/Resources/table.js
@@ -1,9 +1,45 @@
-var tableWin = Ti.UI.createWindow({
-	backgroundColor: 'white'
-});
+var loadData = require('data');
 
+
+var tableWin = Ti.UI.createWindow({
+	backgroundColor: 'white',
+	title: 'Galaxies'
+});
 var tableNav = Ti.UI.iOS.createNavigationWindow({
 	window: tableWin
 });
+
+var mySection = [];
+
+var tableView = Ti.UI.createTableView({
+	headerTitle: 'List of Galaxies'
+});
+
+for (n in loadData.galaxies){
+	var section = Ti.UI.createTableViewSection({
+		headerTitle: n
+	});
+	if (section.headerTitle === 'named'){
+		section.headerTitle = 'Famous Galaxies';
+	} else {
+		section.headerTitle = 'Visible with Naked Eye';
+	}
+	for (var g = 0; g < loadData.galaxies[n].length; g++){
+		var row = Ti.UI.createTableViewRow({
+			title: loadData.galaxies[n][g].title,
+			infoDetail: loadData.galaxies[n][g].description,
+			hasChild: true
+		});
+		section.add(row);
+	}
+	mySection.push(section);
+}
+
+tableView.setData(mySection);
+tableWin.add(tableView);
+
+
+//console.log(loadData.galaxies.nakedEye[0].image);
+//console.log(loadData.galaxies.named.length);
 
 exports.tableNav = tableNav;
